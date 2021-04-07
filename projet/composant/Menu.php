3f1c1de294a1d403c3d31d482ee08e4cf7ce18e5
unknown <lpham-vu@gamingcampus.local>
2021-04-07 10:11:22 +0200

    reparation Menu

--- projet/composant/Menu.php
+++ projet/composant/Menu.php
@@ -1,14 +1,15 @@
 <nav>
     <div class="navbar">
-        <a href=""><img src="../img/logo_redstone.png" alt="Red Stone"></a>
+        
         <ul id="nav">
+            <li><a href=""><img src="../img/logo_redstone.png" alt="Red Stone"></a></li>
             <li><a href="">S'inscrire</a></li>
             <li><a href="">Se Connecter</a></li>
-            <li><form><div class="input-field">
+            <li><div class="input-field">
                 <input id="search" type="search" required>
                 <label class="label-icon" for="search"><i class="material-icons">search</i></label>
                 <i class="material-icons">close</i>
-            </div></form></li>
+            </div></li>
         </ul>
     </div>
 </nav>
