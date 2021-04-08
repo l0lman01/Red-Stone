ea26ef4bec2ce917c32b6e447128cfecd446b59d
unknown <lpham-vu@gamingcampus.local>
2021-04-08 10:32:35 +0200

    menu display

--- projet/composant/Menu.php
+++ projet/composant/Menu.php
@@ -1,10 +1,7 @@
 <nav>
     <div class="navbar">
-        <ul id="nav">
-            <li><a href=""><img src="../img/logo_redstone.png" alt="Red Stone"></a></li>
-            <li><a href="inscription.php">S'inscrire</a></li>
-            <li><a href="connexion.php">Se Connecter</a></li>
         <a href="" class="side"><i class="material-icons">menu</i></a>
+        <a href=""><img src="../img/logo_redstone.png" alt="Red Stone"></a>
         <ul id="nav" class="right hide-on-med-and-down">
             <li><div class="input-field">
                 <input id="search" type="search" required>
