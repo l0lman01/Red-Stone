8c4d82609cb2bc7f8172851a5ee4d0ee041ee545
unknown <lpham-vu@gamingcampus.local>
2021-04-09 14:37:58 +0200

    index carousel presque réussi

--- projet/public/index.php
+++ projet/public/index.php
@@ -6,11 +6,32 @@
     </head>
     <body>
     <?php require "../composant/Menu.php" ?>
+    <section>
+        <div class="container">
+            <div class="carousel">
+                <div classe="row">
+                    <div class="s6">
+                        <div class="carousel-item" href=""><img src="../img/kny_bg.jpg" alt="Red Stone Kny"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis id facilis adipisci incidunt velit ex, debitis recusandae harum laboriosam atque tempore temporibus nisi ab, reiciendis dolore, laborum similique deserunt reprehenderit.</p></div>
 
-    <div class="carousel carousel-slider">
-        <div class="carousel-item" href=""><img src="../img/kny_bg.jpg" alt="Red Stone Kny"></div>
-        <div class="carousel-item" href=""><img src="../img/knb.jpg" alt="Red Stone Kny"></div>
-    </div>
+                    </div>
+                    <div class="carousel-item" href=""><img src="../img/knb.jpg" alt="Red Stone Kny"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis id facilis adipisci incidunt velit ex, debitis recusandae harum laboriosam atque tempore temporibus nisi ab, reiciendis dolore, laborum similique deserunt reprehenderit.</p></div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <h2>ANIMES</h2>
+    </section>
+
+    <section>
+        <h2>MANGA</h2>
+    </section>
+
+    <section>
+        <h2>MUSIQUE</h2>
+    </section>
 
     <?php require '../composant/footer.php' ?>
     </body>  
