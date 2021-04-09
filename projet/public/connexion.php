4fba80e8505006f52f88c3728aa821571aa2acbe
unknown <cdonche@gamingcampus.local>
2021-04-09 14:50:30 +0200

    connexion

--- projet/public/connexion.php
+++ projet/public/connexion.php
@@ -5,18 +5,25 @@
     </head>
     <body class='multianimebg'>
         <?php require "../composant/Menu.php" ?>
-      <br><br><br>
-    <div class="row center white">
+<br><br><br>
+    <div class="row center" >
       <form class="center">
-        <div class="row">
-          <div class="input-field col s6">
-            <input id="icon_prefix" type="text" class="validate">
-            <label for="icon_prefix">First Name</label>
-          </div>
-          <div class="input-field col s6">
-            <input id="icon_telephone" type="tel" class="validate">
-          <label for="icon_telephone">Telephone</label>
+        <div class="center row">
+        <div class="col s3"></div> <div class= "col s6 white">
+        <h2 class="white">Login</h2>      
+        <div class="input-field ">
+        <h6 class="left">Nom d'utilisateur</h6>
+        <input id="icon_telephone" placeholder="Ecrivez votre Nom..." type="tel" class="validate">
         </div>
+        <div class="input-field">
+        <h6 class="left">Mot de passe</h6>
+        <input id="icon_telephone" placeholder="Ecrivez votre mot de passe..." type="tel" class="validate">
+        <a href="" class="right">Mot de passe oublier ?</a>
+        <br><br><br>
+        <a class="waves-effect waves-light btn-small red accent-4 ">Login</a>
+        </div>
+        </div>
+        <div class="col s3"></div>
       </div>
       </form>
     </div>
