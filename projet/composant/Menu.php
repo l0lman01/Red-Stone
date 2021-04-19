5c8f66cdeba080df28fee9d4421230aceeaf293e
unknown <lpham-vu@gamingcampus.local>
2021-04-19 14:21:52 +0200

    sidenav v2

--- projet/composant/Menu.php
+++ projet/composant/Menu.php
@@ -29,19 +29,13 @@
 
 
 <ul id="slide-out" class="sidenav">
-  <li><div class="user-view">
-    <div class="background">
-      <img src="images/office.jpg">
-    </div>
-    <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
-    <a href="#name"><span class="white-text name">John Doe</span></a>
-    <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
-  </div></li>
-  <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-  <li><a href="#!">Second Link</a></li>
+  <li><a href="">Accueil</a></li>
   <li><div class="divider"></div></li>
-  <li><a class="subheader">Subheader</a></li>
-  <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  <li><a href="">Animes</a></li>
+  <li><div class="divider"></div></li>
+  <li><a href="">Manga</a></li>
+  <li><div class="divider"></div></li>
+  <li><a href="../prive/profil.php">Profile</a></li>
 </ul>
 
  
--- projet/public/connexion.php
+++ projet/public/connexion.php
@@ -1,39 +1,45 @@
 <html lang="fr">
-    <head>
-        <?php require "../composant/head.php" ?>
-        <title>Red Stone | inscription</title>
-    </head>
-    <body>
-        <?php require "../composant/Menu.php" ?>
-<br><br><br>
-    <div class="row center" >
-      <form class="center">
-        <div class="center row">
-        <div class="col s3"></div> <div class= "col s6 white">
-        <h2 class="white">Login</h2>      
-        <div>
-        <h6 class="left">Nom d'utilisateur</h6>
-        <input id="icon_telephone" placeholder="Ecrivez votre Nom..." type="tel" class="validate">
-        </div>
-        <div>
-        <h6 class="left">Mot de passe</h6>
-        <input id="icon_telephone" placeholder="Ecrivez votre mot de passe..." type="tel" class="validate">
-        <a href="" class="right">Mot de passe oublier ?</a>
-        <br><br><br>
-        <a class="waves-effect waves-light btn-small red accent-4 ">Login</a>
-        <br><br>
-        <h5>Connectez vous avec</h5>
-        <div>
-        <img src="../img/logo-fb.png" alt="" with=150px height=150px>
-        <img src="../img/logo-google.png" alt="" with=150px height=150px>
-        <h6>pas encore inscrits ?</h6><a href="">Inscrivez-vous maintenant</a>
-        </div>
-        </div>
-        </div>
-        <div class="col s3"></div>
-      </div>
-      </form>
-    </div>
+   <head>
+      <?php require "../composant/head.php" ?>
+      <title>Red Stone | inscription</title>
+   </head>
+   <body>
+      <?php require "../composant/Menu.php" ?>
+      <br><br><br>
+      <div class="row center" >
+         <form class="center">
+            <div class="center row">
+               <div class="col s3"></div>
+               <div class= "col s6 white">
+                  <h2 class="white">Login</h2>
+                  <div>
+                     <h6 class="left">Nom d'utilisateur</h6>
+                     <input id="icon_telephone" placeholder="Ecrivez votre Nom..." type="tel" class="validate">
+                  </div>
 
-    </body>
+                  <div>
+                     <h6 class="left">Mot de passe</h6>
+                     <input id="icon_telephone" placeholder="Ecrivez votre mot de passe..." type="tel" class="validate">
+                     <a href="" class="right">Mot de passe oublier ?</a>
+
+                     <br><br><br>
+
+                     <a class="waves-effect waves-light btn-small red accent-4 ">Login</a>
+
+                     <br><br>
+                     
+                     <h5>Connectez vous avec</h5>
+                     <div>
+                        <img src="../img/logo-fb.png" alt="" with=150px height=150px>
+                        <img src="../img/logo-google.png" alt="" with=150px height=150px>
+                        <h6>pas encore inscrits ?</h6>
+                        <a href="">Inscrivez-vous maintenant</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col s3"></div>
+            </div>
+         </form>
+      </div>
+   </body>
 </html>
