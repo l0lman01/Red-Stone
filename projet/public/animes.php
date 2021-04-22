adb482fc8bdc0ccdfdd3daccf585b26c63c5d5b8
unknown <lpham-vu@gamingcampus.local>
2021-04-22 17:02:48 +0200

    corection page anime

--- projet/public/animes.php
+++ projet/public/animes.php
@@ -46,7 +46,7 @@
                     <div class='col s12'>
                         <div class="card horizontal">
                             <div class="card-image">
-                                <img src="../img/upload/BOFURI.jpg" alt="Red Stone Bofuri">
+                                <img src="../img/imgAnime/BOFURI.jpg" alt="Red Stone Bofuri">
                             </div>
                             <div class="card-content">
                                 <span class="card-title">Bofuri</span>
@@ -85,21 +85,23 @@
         </div>
 
         <div class="carousel">
-        <div class="carousel-item row ">
-                <div class="col s12 m2">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="../img/kny_card_img.jpg">
-                            <span class="card-title">Demon Slayer</span>
+            <div class="carousel-item ">
+                <div class="row">
+                    <div class="col s12">
+                        <div class="card large">
+                            <div class="card-image">
+                                <img src="../img/kny_card_img.jpg">
+                                <span class="card-title">Demon Slayer</span>
+                            </div>
+                            <div class="card-content">
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="#">This is a link</a>
+                            </div>
                         </div>
-                        <div class="card-content">
-                            <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="#">This is a link</a>
-                        </div>
-                    </div>
+                    </div>    
                 </div>
         </div>
 
