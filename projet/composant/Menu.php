f9dec9f1872f235a6f6f1875918c83a1edbb08bd
unknown <lpham-vu@gamingcampus.local>
2021-04-22 15:45:49 +0200

    boutons sur sideNav

--- projet/composant/Menu.php
+++ projet/composant/Menu.php
@@ -23,7 +23,6 @@
               <li><a href="../prive/profil.php" class="button">Mon Profile</a></li>
               <li><a href="../actions/logout.php" class="button">Déconnexion</a></li>
               <?php } ?>
-
         </ul>
     </div>
 </nav>
@@ -37,4 +36,18 @@
   <li><a href="../public/manga.php">Manga</a></li>
   <li><div class="divider"></div></li>
   <li><a href="../prive/profil.php">Profile</a></li>
+  <?php 
+    if(empty($_SESSION['user'])){ 
+  ?>
+    <li><a href="../public/inscription.php" class="button">S'inscrire</a></li>
+    <li><a href="../public/connexion.php" class="button">Se Connecter</a></li>
+  <?php }else{
+    $user = $_SESSION['user'];
+    if($user['statut'] == 'admin'){  
+  ?>
+      <li><a href="../prive/panel.php" class="button">Panel Admin</a></li>
+  <?php } ?>                
+    <li><a href="../prive/profil.php" class="button">Mon Profile</a></li>
+    <li><a href="../actions/logout.php" class="button">Déconnexion</a></li>
+  <?php } ?>
 </ul>
