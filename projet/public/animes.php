4859ca0185e41376003389e9ed6ea5c663d5694c
unknown <cdonche@gamingcampus.local>
2021-04-22 16:00:07 +0200

    car anime

--- projet/public/animes.php
+++ projet/public/animes.php
@@ -7,14 +7,101 @@
     </head>
     <body>
         <?php require "../composant/Menu.php" ?>
+        <br<br>
+        <div class="carousel carousel-slider">                  
+            <div class="carousel-item" href="">
+                <div class='row'>
+                    <div class='col s12'>
+                        <div class="card horizontal">
+                            <div class="card-image">
+                                <img src="../img/kny_bg.jpg" alt="Red Stone KnY">
+                            </div>
+                            
+                            <div class="card-content">
+                                <span class="card-title">Demon Slayer</span>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis id facilis adipisci incidunt velit ex, debitis recusandae harum laboriosam atque tempore temporibus nisi ab, reiciendis dolore, laborum similique deserunt reprehenderit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item" href="">
+                <div class='row'>
+                    <div class='col s12'>
+                        <div class="card horizontal">
+                            <div class="card-image">
+                                <img src="../img/violet.jpg" alt="Red Stone Violet Evergarden">
+                            </div>
+                            
+                            <div class="card-content">
+                                <span class="card-title">Violet Evergarden</span>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis id facilis adipisci incidunt velit ex, debitis recusandae harum laboriosam atque tempore temporibus nisi ab, reiciendis dolore, laborum similique deserunt reprehenderit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item" href="">
+                <div class='row'>
+                    <div class='col s12'>
+                        <div class="card horizontal">
+                            <div class="card-image">
+                                <img src="../img/upload/BOFURI.jpg" alt="Red Stone Bofuri">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title">Bofuri</span>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis id facilis adipisci incidunt velit ex, debitis recusandae harum laboriosam atque tempore temporibus nisi ab, reiciendis dolore, laborum similique deserunt reprehenderit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item" href="">
+                <div class='row'>
+                    <div class='col s12'>
+                        <div class="card horizontal">
+                            <div class="card-image">
+                                <img src="../img/knb.jpg" alt="Red Stone KnB">
+                            </div>
+                            
+                            <div class="card-content">
+                                <span class="card-title">Kuroko no Basket</span>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis id facilis adipisci incidunt velit ex, debitis recusandae harum laboriosam atque tempore temporibus nisi ab, reiciendis dolore, laborum similique deserunt reprehenderit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div>
+                <div class="col s1 m1"></div><div>
+                    <h2 class="red-text text-accent-4">Anime</h2>
+                </div>
+                <div class="row">
+                    <div class="col s1 m1 "></div><div class="col s8 m8 divider"></div><div class="col s3 m3"></div>
+                </div>
+            </div>
+        </div>
 
-        
-
-
-
-
-
-
+        <div class="carousel">
+        <div class="carousel-item row ">
+                <div class="col s12 m2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../img/kny_card_img.jpg">
+                            <span class="card-title">Demon Slayer</span>
+                        </div>
+                        <div class="card-content">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">This is a link</a>
+                        </div>
+                    </div>
+                </div>
+        </div>
 
 
 
