fc4510cecba868c47471f8c18750d88b0be5c733
unknown <tgoris@gamingcampus.local>
2021-04-23 15:25:37 +0200

    ajout manga

---
+++ projet/composant/addMangaModal.php
@@ -0,0 +1,25 @@
+<div id="modal2" class="modal">
+    <div class="modal-content">
+      <form action="../actions/newManga.php" method='post' enctype="multipart/form-data">
+        <div>
+            <label>Nom du manga</label>
+            <input type="text" name='name' required>
+        </div>
+        <br/>
+        <div>
+            <label>Image</label>
+            <br/>
+            <input type="file" name="image" required>
+        </div>
+        <div>
+            
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <button class="btn waves-effect waves-light" type="submit" name="action" >
+          <i class="far fa-plus-square"></i>
+       </button>
+      </form>
+    </div>
+  </div>
