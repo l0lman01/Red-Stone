17d78ceee273de7a9f55127dd758c5c91c66245f
unknown <cdonche@gamingcampus.local>
2021-04-23 17:17:23 +0200

    debut page anime

---
+++ projet/public/anime.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <?php require "../composant/head.php" ?>
+    <title>Red Stone-Animes</title>
+</head>
+
+<body>
+    <?php require "../composant/Menu.php" ?>
+    <br><br>
+    <div class="row">
+        <div class="en-tete">
+            <div class="col m1"></div>
+            <div class="col s12 m3">
+                <br><br>
+                <img class="couverture" src="../img/Haikyu.jpg" alt="">
+            </div>
+
+            <div class= "col m8 center" >
+                <div class= "col m12 center" >
+                    <img class="logoAnime" src="../img/Haikyuu_logo.png" alt="">
+                </div>
+                <div class="col m1"></div>
+                <div class="col m10 glip">
+                    <div class="black bubble center">
+                        <li class="tab"><a class="white-text" href="#animess">Animes suivis</a></li>
+                    </div>
+                    <div class="fru"></div>
+                    <div class="black bubble center">
+                        <li class="tab"><a class="white-text" href="#animess">Animes suivis</a></li>
+                    </div>
+                    <div class="fru"></div>
+                    <div class="black bubble center">
+                        <li class="tab"><a class="white-text" href="#animess">Animes suivis</a></li>
+                    </div>
+                <div class="col m1"></div>
+                
+                </div>
+            </div>
+        </div>
+    
+
+
+
+
+
+    </div>
+    </container>
+
+
+
+
+
+
+
+
+
+    <?php require '../composant/footer.php' ?>
+</body>
+
+</html>
