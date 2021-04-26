e7c698eb424bc688ecb4f1a7949906f3e4fc971b
xproavo <xpro.avo@gmail.com>
2021-04-26 03:44:52 +0200

    modif css

--- projet/public/payment.php
+++ projet/public/payment.php
@@ -9,10 +9,9 @@
     <body>
         <?php require '../composant/Menu.php' ?>
 
-        <div>
-            <div class="rowflex arround-md">
-
-                <div class="cols-xs-12 cols-md-5 " >
+        <div class="payementbg">
+            <div class="rowflex" >
+                <div class="cols-xs-12 cols-md-6 " >
 
                     <div class="box white payement" >
                         <h5>1. Votre compte</h5>
@@ -40,10 +39,48 @@
 
 
 
-                <div class="cols-xs-12 cols-md-5 white" style="margin: 20px;">
-                    <div class="box">
+                <div class="cols-xs-12 cols-md-6">
+                    <div class="box white payement">
                         <h3>Montant:</h3>
                         
+                        <?php  
+                            if($_GET['offre'] == 1){
+                                $offre = 'Mensuel';
+                                $prix = 'xx.xx';
+                                $reduction = 'xx.xx';
+                                $total = 'xx.xx';
+                            } else if ($_GET['offre'] == 2){
+                                $offre = '3 Mois';
+                                $prix = 'xx.xx';
+                                $reduction = 'xx.xx';
+                                $total = 'xx.xx';
+                            } else if ($_GET['offre'] == 3){
+                                $offre = 'Annuel';
+                                $prix = 'xx.xx';
+                                $reduction = 'xx.xx';
+                                $total = 'xx.xx';
+                            }
+                        ?>
+
+                        <div class="rowflex">
+                            <h6 class="col-xs-8">Abonnement VIP <?php echo $offre ?> :</h6>
+                            <h6 class="col-xs-4"><?php echo $prix ?>€</h6>
+                        </div>
+                        <hr/>
+                        <div class="rowflex">
+                            <p class="col-xs-8">Sous-total :</p>
+                            <p class="col-xs-4"><?php echo $prix ?>€</p>
+                        </div>
+                        <div class="rowflex">
+                            <p class="col-xs-8">Réduction</p>
+                            <p class="col-xs-4">-<?php echo $reduction ?>€</p>
+                        </div>
+                        <hr/>
+                        <div class="rowflex">
+                            <h3 class="col-xs-8"> <strong> Total </strong></h3>
+                            <h3 class="col-xs-4"><strong><?php echo $total ?>€</strong></h3>
+                        </div>
+
                     </div>
                 </div>
 
